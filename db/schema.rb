e7db18c70765198b85d42efc0fd94dc5078877c9
yuyaamano23 <nba.chako.nba@icloud.com>
2020-05-25 02:59:59 +0000

    reaction-model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_23_091009) do
+ActiveRecord::Schema.define(version: 2020_05_25_025641) do
+
+  create_table "reactions", force: :cascade do |t|
+    t.integer "to_user_id", null: false
+    t.integer "from_user_id", null: false
+    t.integer "status", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
